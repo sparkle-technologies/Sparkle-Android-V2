f6f9c2a3d82f28bed52d57e0930608398f9a008b
BlackJackChenn <blackjackchen@sparkle.fun>
2023-08-31 17:51:52 +0800

    feat: register page, select date/time widget

--- app/src/main/java/com/cyberflow/sparkle/register/view/SelectBirthdayFragment.kt
+++ app/src/main/java/com/cyberflow/sparkle/register/view/SelectBirthdayFragment.kt
@@ -9,6 +9,11 @@ import com.cyberflow.sparkle.databinding.FragmentSelectBirthdayBinding
 import com.cyberflow.sparkle.login.viewmodel.LoginRegisterViewModel
 import com.cyberflow.sparkle.login.widget.ShadowImgButton
 import com.cyberflow.sparkle.login.widget.ShadowTxtButton
+import com.cyberflow.sparkle.register.widget.daytimepicker.CustomDatePicker
+import com.cyberflow.sparkle.register.widget.daytimepicker.DateFormatUtils
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class SelectBirthdayFragment :
     BaseVBFragment<LoginRegisterViewModel, FragmentSelectBirthdayBinding>() {
@@ -42,15 +47,77 @@ class SelectBirthdayFragment :
         })
     }
 
+
+    private var mDatePicker: CustomDatePicker? = null
+    private var selectDate: String = ""
+    private var selectTime: String = ""
+
+
     private fun selectDate() {
         Log.e("TAG", "selectDate: ")
-        //todo select data widget
-        mViewBind.etBirthDate.setText("2000-01-01")
+        initDatePicker()
+        // yyyy-MM-dd
+        if (selectDate.isNotEmpty()) {
+            mDatePicker?.show(selectDate)
+        } else {
+            mDatePicker?.show(DateFormatUtils.long2Str(System.currentTimeMillis(), false))
+        }
     }
 
     private fun selectTime() {
         Log.e("TAG", "selectTime: ")
-        mViewBind.etBirthTime.setText("00:00")
+        initTimePicker()
+        mDatePicker?.show("2023-11-11 ${selectTime.ifEmpty { "00:00:00" }}")
     }
 
+    private fun initDatePicker() {
+        val beginTimestamp: Long = DateFormatUtils.str2Long("1970-02-01", false)
+        val endTimestamp = System.currentTimeMillis()
+
+        // 通过时间戳初始化日期，毫秒级别
+        mDatePicker = CustomDatePicker(
+            context, { timestamp ->
+                selectDate = DateFormatUtils.long2Str(timestamp, false)
+                mViewBind.etBirthDate.setText(selectDate)
+            },
+            beginTimestamp,
+            endTimestamp
+        ).apply {
+            setTitle("Set Date");
+            setCancelable(true)
+            setCanShowPreciseTime(false)
+            setScrollLoop(false)
+            setCanShowAnim(false)
+        }
+    }
+
+    private fun initTimePicker() {
+        val beginTime = "2023-11-11 00:00"
+        val endTime = "2023-11-11 23:59"
+
+        // 通过时间戳初始化日期，毫秒级别
+        mDatePicker =
+            CustomDatePicker(
+                context,
+                { timestamp ->
+                    val date = Date(timestamp)
+                    selectTime = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(date)
+                    mViewBind.etBirthTime.setText(
+                        SimpleDateFormat(
+                            "HH:mm",
+                            Locale.getDefault()
+                        ).format(date)
+                    )
+                },
+                beginTime,
+                endTime
+            ).apply {
+                setTitle("Set Time")
+                setCancelable(true)
+                setCanShowPreciseDate(false)
+                setCanShowPreciseTime(true)
+                setScrollLoop(false)
+                setCanShowAnim(false)
+            }
+    }
 }
