049d03ef5df3bc0c49bb70f50e8b942893bfc1d1
BlackJackChenn <blackjackchen@sparkle.fun>
2023-09-13 17:37:58 +0800

    refactor: animation for Main Tab clicked

--- app/src/main/java/com/cyberflow/sparkle/main/view/MainActivity.kt
+++ app/src/main/java/com/cyberflow/sparkle/main/view/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.cyberflow.sparkle.main.view
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.util.Log
-import androidx.core.content.res.ResourcesCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.cyberflow.base.act.BaseDBAct
+import com.cyberflow.base.util.dp2px
 import com.cyberflow.sparkle.databinding.ActivityMainBinding
 import com.cyberflow.sparkle.main.viewmodel.MainViewModel
 import com.cyberflow.sparkle.register.view.PageAdapter
@@ -12,19 +13,37 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : BaseDBAct<MainViewModel, ActivityMainBinding>() {
 
+    private var dis = 0f
+    private var fromLeft2Right : ObjectAnimator? = null
+    private var fromRight2Left : ObjectAnimator? = null
+    private var lastLeft = false
+    private var lastRight = false
+
     private fun clickTopMenu(left: Boolean, right: Boolean) {
         if(left && right) return // avoid same click
-        mDataBinding.bgMenuLeft.background = if (left) ResourcesCompat.getDrawable(resources, com.cyberflow.base.resources.R.drawable.main_bg_top_menu_selected, null) else null
-        mDataBinding.bgMenuRight.background = if (right) ResourcesCompat.getDrawable(resources, com.cyberflow.base.resources.R.drawable.main_bg_top_menu_selected, null) else null
+        if(lastRight == right && lastLeft == left) return
+        lastLeft = left
+        lastRight = right
+
+        if(left){
+            fromRight2Left?.start()
+        }
+
+        if(right){
+            fromLeft2Right?.start()
+        }
+
         mDataBinding.ivMenuLeft.setImageResource((if (left) com.cyberflow.base.resources.R.drawable.svg_ic_horoscope_select else com.cyberflow.base.resources.R.drawable.svg_ic_horoscope_unselect))
         mDataBinding.ivMenuRight.setImageResource((if (right) com.cyberflow.base.resources.R.drawable.svg_ic_contact_select else com.cyberflow.base.resources.R.drawable.svg_ic_contact_unselect))
         if(left) goPrevious()
         if(right)  goNext()
     }
 
+
+
     override fun initView(savedInstanceState: Bundle?) {
-        mDataBinding.bgMenuLeft.setOnClickListener { clickTopMenu(true, false) }
-        mDataBinding.bgMenuRight.setOnClickListener { clickTopMenu(false, true) }
+        mDataBinding.viewMenuLeft.setOnClickListener { clickTopMenu(true, false) }
+        mDataBinding.viewMenuRight.setOnClickListener { clickTopMenu(false, true) }
         mDataBinding.ivHead.setOnClickListener {
             Snackbar.make(mDataBinding.ivHead, "click me", Snackbar.LENGTH_SHORT).show()
         }
@@ -42,6 +61,10 @@ class MainActivity : BaseDBAct<MainViewModel, ActivityMainBinding>() {
             orientation = ViewPager2.ORIENTATION_HORIZONTAL
             this.adapter = adapter
         }
+
+        dis = dp2px(82f).toFloat()
+        fromLeft2Right = ObjectAnimator.ofFloat(mDataBinding.bgMenuLeft, "translationX", 0f, dis)
+        fromRight2Left = ObjectAnimator.ofFloat(mDataBinding.bgMenuLeft, "translationX", dis, 0f)
 
         clickTopMenu(true, false)
     }
