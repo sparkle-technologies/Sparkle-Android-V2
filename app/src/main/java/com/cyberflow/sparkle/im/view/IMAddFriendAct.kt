ec2756a3a28d4ef257ace911a4a84a20b89bbacf
BlackJackChenn <blackjackchen@sparkle.fun>
2023-09-27 19:57:01 +0800

    feat: IM contacts adapter UI

--- app/src/main/java/com/cyberflow/sparkle/im/view/IMAddFriendAct.kt
+++ app/src/main/java/com/cyberflow/sparkle/im/view/IMAddFriendAct.kt
@@ -5,10 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import com.cyberflow.base.act.BaseDBAct
 import com.cyberflow.base.model.IMSearchData
+import com.cyberflow.sparkle.chat.common.interfaceOrImplement.OnResourceParseCallback
 import com.cyberflow.sparkle.databinding.ActivityImAddFriendBinding
 import com.cyberflow.sparkle.im.viewmodel.IMViewModel
 import com.cyberflow.sparkle.login.widget.ShadowTxtButton
+import com.cyberflow.sparkle.main.viewmodel.parseResource
 import com.drake.net.utils.TipUtils
+import com.google.android.material.snackbar.Snackbar
+import com.vanniktech.ui.hideKeyboard
 
 class IMAddFriendAct : BaseDBAct<IMViewModel, ActivityImAddFriendBinding>() {
 
@@ -23,18 +27,43 @@ class IMAddFriendAct : BaseDBAct<IMViewModel, ActivityImAddFriendBinding>() {
     }
 
     override fun initView(savedInstanceState: Bundle?) {
-        mDataBinding.btnSend.setClickListener(object : ShadowTxtButton.ShadowClickListener{
+        mDataBinding.btnSend.setClickListener(object : ShadowTxtButton.ShadowClickListener {
             override fun clicked(disable: Boolean) {
-                 TipUtils.toast("go add friend")
+                submit()
             }
         })
     }
 
+    private var searchName = ""
 
     override fun initData() {
-        (intent.getSerializableExtra(TAG) as? IMSearchData)?.apply {
+        (intent.getSerializableExtra(TAG) as? IMSearchData)?.apply{
+            searchName = name
             mDataBinding.tvName.text = name
             mDataBinding.tvAddress.text = address
         }
+
+        viewModel.friendObservable.observe(this) { response ->
+            parseResource(response, object : OnResourceParseCallback<Boolean>() {
+                override fun onSuccess(data: Boolean?) {
+                    if (data == true) {
+                        Snackbar.make(mDataBinding.ivHead, "request send successfully", Snackbar.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        TipUtils.toast("failed to send request")
+                    }
+                }
+            })
+        }
+    }
+
+    private fun submit() {
+        val msg = mDataBinding.et.text.toString()
+        if (msg.isEmpty()) {
+            TipUtils.toast("please Enter authentication messaged")
+            return
+        }
+        hideKeyboard()
+        viewModel.addFriend(searchName, msg)
     }
 }
--- app/src/main/java/com/cyberflow/sparkle/im/viewmodel/IMViewModel.kt
+++ app/src/main/java/com/cyberflow/sparkle/im/viewmodel/IMViewModel.kt
@@ -1,13 +1,23 @@
 package com.cyberflow.sparkle.im.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import com.cyberflow.base.model.LoginResponseData
-import com.cyberflow.base.model.RegisterRequestBean
+import android.util.Log
+import androidx.lifecycle.MediatorLiveData
 import com.cyberflow.base.viewmodel.BaseViewModel
+import com.cyberflow.sparkle.chat.common.net.Resource
+import com.cyberflow.sparkle.chat.common.repositories.EMContactManagerRepository
 
 class IMViewModel : BaseViewModel() {
 
-    var userInfo: MutableLiveData<LoginResponseData> = MutableLiveData()
+    val mContactRepository = EMContactManagerRepository()
 
+    var friendObservable: MediatorLiveData<Resource<Boolean>> = MediatorLiveData()
+    fun addFriend(username: String, reason: String) {
+        Log.e("TAG", "addFriend: username=$username reason=$reason")
+        mContactRepository?.also {
+            friendObservable?.addSource(it.addContact(username, reason)) { response ->
+                friendObservable.value = response
+            }
+        }
+    }
 
 }
