46350485f200639e74cea38b0251620005e9029f
BlackJackChenn <blackjackchen@sparkle.fun>
2023-11-08 19:54:02 +0800

    refactor: generate apk file after import flutter module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
             )
         }
     }
+    
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
